5a2acc0909a904282c96f03b95aac5532c01844f
matsu-chara <matsuy00@gmail.com>
2015-05-31 15:34:38 +0900

    Update HListQuickSort.scala

--- src/main/scala-2.11/HList/HListQuickSort.scala
+++ src/main/scala-2.11/HList/HListQuickSort.scala
@@ -3,27 +3,6 @@ package HList
 import Nat.Nat
 import HListAppend._
 
-//trait HListQuickSort[L <: HList, Out <: HList]
-//
-//object HListQuickSort {
-//
-//  // if true then: sorted(HNil) should_be HNil
-//  implicit def hnilSorted = new HListQuickSort[HNil, HNil] {}
-//
-//  implicit def hlistSorted[H <: Nat, T <: HList,
-//  lsOut <: HList, gsOut <: HList, smOut <: HList, slOut <: HList,
-//  prepOut <: HList]
-//  (implicit
-//   ls: LTEqs[T, H, lsOut],
-//   gs: GTs[T, H, gsOut],
-//   sortedSmaller: HListQuickSort[lsOut, smOut],
-//   sortedLarger: HListQuickSort[gsOut, slOut],
-//   preps: HAppend[smOut, H :: slOut, prepOut]) =
-//    new HListQuickSort[H :: T, prepOut] {}
-//
-//  def apply[L <: HList, M <: HList](implicit sorted: HListQuickSort[L, M]) = sorted
-//}
-
 trait HListQuickSort[L <: HList] {
   type Out <: HList
 }
